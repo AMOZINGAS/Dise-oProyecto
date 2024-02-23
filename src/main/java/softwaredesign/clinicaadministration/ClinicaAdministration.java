a474cd52710bd229f35b01ab5577b30b647b2b11
Amós Olguin <>
2024-02-22 22:51:50 -0600

    ProyectoBase

---
+++ src/main/java/softwaredesign/clinicaadministration/ClinicaAdministration.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package softwaredesign.clinicaadministration;
+
+/**
+ *
+ * @author PC
+ */
+public class ClinicaAdministration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
