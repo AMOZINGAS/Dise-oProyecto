431ab87f214ef430c2e2dac1b9967b94455547ba
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-03-14 11:32:14 -0700

    De Todo se hizo

---
+++ src/clinicappointment/ClinicAppointment.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clinicappointment;
+
+import presentation.JFrameLogin;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class ClinicAppointment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JFrameLogin login = new JFrameLogin();
+        login.setVisible(true);
+        
+    }
+    
+}
